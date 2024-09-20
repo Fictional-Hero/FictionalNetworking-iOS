8541392375c247e214fa2ac290af758c592b87cf
Fictional-Hero <68499521+Fictional-Hero@users.noreply.github.com>
2024-09-20 16:23:10 +0200

    first commit

---
+++ FictionalNetworkingSample/FictionalNetworkingSample/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FictionalNetworkingSample
+//
+//  Created by Joshua Erasmus on 2024/09/20.
+//
+
+import SwiftUI
+import FictionalNetworking
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FictionalNetworkingSample/FictionalNetworkingSample/FictionalNetworkingSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FictionalNetworkingSampleApp.swift
+//  FictionalNetworkingSample
+//
+//  Created by Joshua Erasmus on 2024/09/20.
+//
+
+import SwiftUI
+
+@main
+struct FictionalNetworkingSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
